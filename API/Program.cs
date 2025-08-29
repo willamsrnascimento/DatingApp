31acdb712c00f245b1519c6a3147a1a973afb5ee
Willams Reis <willams.trb@gmail.com>
2025-08-29 08:19:37 -0300

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
