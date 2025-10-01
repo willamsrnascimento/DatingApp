671b139eeafa93f107e5288138efe62845cfac45
Willams Reis <willams.trb@gmail.com>
2025-10-01 09:26:46 -0300

    End of  section 6

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MembersList } from '../features/members/members-list/members-list';
+import { MembersDetailed } from '../features/members/members-detailed/members-detailed';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MembersList },
+            { path: 'members/:id', component: MembersDetailed },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+        ]
+    },
+    { path: '**', component: Home },
+];
---
+++ client/src/features/members/members-detailed/members-detailed.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-members-detailed',
+  imports: [],
+  templateUrl: './members-detailed.html',
+  styleUrl: './members-detailed.css'
+})
+export class MembersDetailed {
+
+}
